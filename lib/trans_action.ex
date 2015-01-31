2023fdc60ace3c23876cac8e55388c97447f2bcb
Ivy Rogatko <ipr210lehigh@gmail.com>
2015-01-31 11:09:10 -0800

    overloading and 9999 but working and lots of commented out prints

--- lib/trans_action.ex
+++ lib/trans_action.ex
@@ -1,33 +1,28 @@
 use DB
 
 defmodule Trans_action do
-	#import ExProf.Macro
-
 	def write_read(name, email, message, id) do
-		#profile do
-		Amnesia.transaction! do
-			person = %User{name: name, email: email} |> User.write
-			# Now let's add some messages.
+#find out how to list all the items in the database
+		for n <- 1..id do
+			Amnesia.transaction! do
+				#put in a loop and use counter to modify name and email and msg
+				person = %User{name: name <> Integer.to_string(n), email: email <> Integer.to_string(n)} |> User.write
+				# Now let's add some messages.
+				#IO.puts person.email
+				#IO.puts person.id
+				person |> User.add_message(message <> Integer.to_string(n))
 
-			person |> User.add_message message
+				person = User.read(person.id)
 
-	  	end
-	  	# IO.puts name
-	  	# IO.puts email
-	  	# IO.puts message
-	  	# IO.puts id
-	  	IO.puts "woot"
-	  	
-	  	Amnesia.transaction! do
-		   	person = User.read(id)
-
-			# # Now let's read his messages and print them all.
-			person |> User.messages |> Enum.each &IO.puts(&1.content)
-
-	  		IO.puts "in transaction"
-	  	end
-
-	  	IO.puts "woot after trans"
-	  	#end
+				# # Now let's read his messages and print them all.
+				#person |> User.messages |> Enum.each &IO.puts(&1.content)
+		  	end
+		end
+		  	# IO.puts name
+		  	# IO.puts email
+		  	# IO.puts message
+		  	# IO.puts id
+		  	#IO.puts "woot"
+	  	#IO.puts "woot after trans"
 	end
 end
--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -7,11 +7,17 @@ defmodule HelloPhoenix.PageController do
 
   plug :action
 
+#defmodule Trans_action has a decent amount of prints and the below can overflow a bit (too many db tables)
+#uses list comprehension for loop also will do with recursion eventually...
   def index(conn, _params) do
   	profile do
-  		pid1 = spawn(Trans_action, :write_read, ["John", "j@example.com", "hiiii1", 1])
-  		pid2 = spawn(Trans_action, :write_read, ["Bob", "b@example.com", "hiiii2", 2])
-  		pid3 = spawn(Trans_action, :write_read, ["Carl", "c@example.com", "hiiii3", 3])
+
+	  	#below causes an overflow of too many DB tables but left in since interesting
+	  	#for n <- 1..3333 do
+	  		pid1 = spawn(Trans_action, :write_read, ["John", "j@example.com", "hiiii1", 3333]) #replace 3333 with n for overflow
+	  		pid2 = spawn(Trans_action, :write_read, ["Bob", "b@example.com", "hiiii2", 3333])  #replace 3333 with n for overflow
+	  		pid3 = spawn(Trans_action, :write_read, ["Carl", "c@example.com", "hiiii3", 3333]) #replace 3333 with n for overflow
+	  	#end
   	end
   	#IO.puts trans_block
 
