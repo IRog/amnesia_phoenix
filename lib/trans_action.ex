d64fdac73b7a71958714cffcb878488771ed7cb9
Ivy Rogatko <ipr210lehigh@gmail.com>
2015-02-01 17:22:13 -0800

    better version

--- lib/trans_action.ex
+++ lib/trans_action.ex
@@ -2,19 +2,17 @@ use DB
 
 defmodule Trans_action do
 	def write_read(name, email, message, id) do
-		for n <- 1..id do #comment out and replace n with id for [error] ** Too many db tables ** if do 10k processes in controller
-			Amnesia.transaction! do
-				person = %User{name: name <> Integer.to_string(n), email: email <> Integer.to_string(n)} |> User.write
-				#IO.puts person.email
-				#IO.puts person.id
-				person |> User.add_message(message <> Integer.to_string(n))
+		Amnesia.transaction! do
+			person = %User{name: name <> Integer.to_string(id), email: email <> Integer.to_string(id)} |> User.write
+			#IO.puts person.email
+			#IO.puts person.id
+			person |> User.add_message(message <> Integer.to_string(id))
 
-				person = User.read(person.id)
+			person = User.read(person.id)
 
-				# Now let's read his messages and print them all.
-				#person |> User.messages |> Enum.each &IO.puts(&1.content)
-		  	end
-		end
+			# # Now let's read his messages and print them all.
+			#person |> User.messages |> Enum.each &IO.puts(&1.content)
+	  	end
 		  	# IO.puts name
 		  	# IO.puts email
 		  	# IO.puts message
--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -1,24 +1,20 @@
 defmodule HelloPhoenix.PageController do
   use Phoenix.Controller
-  import Trans_action
   import ExProf.Macro
 
   plug :action
 
   def index(conn, _params) do
   	profile do
-
-	  	#for n <- 1..2500 do #causes more [error] ** Too many db tables ** but left in since 1 process per transaction if comment out compherension in trans_action.ex
-	  		pid1 = spawn(Trans_action, :write_read, ["John", "j@example.com", "hiiii1", 2500]) #replace 2500 with n for more overflow warnings
-	  		pid2 = spawn(Trans_action, :write_read, ["Bob", "b@example.com", "hiiii2",  2500]) #replace 2500 with n for more overflow warnings
-	  		pid3 = spawn(Trans_action, :write_read, ["Carl", "c@example.com", "hiiii3", 2500]) #replace 2500 with n for more overflow warnings
-	  		pid4 = spawn(Trans_action, :write_read, ["Dave", "d@example.com", "hiiii4", 2500]) #replace 2500 with n for more overflow warnings
-	  	#end
+	  	for n <- 1..1000 do
+	  		spawn(Trans_action, :write_read, ["John #{n}", "j#{n}@example.com", "hiiii: ", n])
+	  	end
   	end
+
   	Amnesia.info
 
     conn 
-    |> put_flash(:notice, "dunnno. Need to see what the transaction returns to see if can pass value to flash or do list?")
+    |> put_flash(:notice, "dunnno. Wanted to try a flash message")
     |> render "index.html"
   end
 end
